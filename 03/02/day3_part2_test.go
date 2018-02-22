67968427e2e1b9f2275785b2a466a9eb9eca5c1c
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-02-23 07:36:06 +1100

    WIP on day 3 part 2 - started adding a co-ordinate map to the spiral structure

--- 03/02/day3_part2_test.go
+++ 03/02/day3_part2_test.go
@@ -1,10 +1,11 @@
 /*
-For example:
+So, the first few squares' values are chosen as follows:
 
-Data from square 1 is carried 0 steps, since it's at the access port.
-Data from square 12 is carried 3 steps, such as: down, left, left.
-Data from square 23 is carried only 2 steps: up twice.
-Data from square 1024 must be carried 31 steps.
+Square 1 starts with the value 1.
+Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
 */
 
 package main
