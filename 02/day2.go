225465060dfc3f16d7ed64c2551e604edf1aac47
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-02-22 18:44:03 +1100

    Resized initial

--- 02/day2.go
+++ 02/day2.go
@@ -14,10 +14,10 @@ ConvertInput will parse and rearrange the input provided for the day 2 exercises
 http://adventofcode.com/2017/day/2
 */
 func ConvertInput(input string) [][]int {
-	result := make([][]int, 4)
+	result := make([][]int, 16)
 
 	for row, i := range strings.Split(input, "\n") {
-		result[row] = make([]int, 4)
+		result[row] = make([]int, 16)
 
 		for col, j := range strings.Split(i, "\t") {
 			convInt, err := strconv.Atoi(strings.TrimSpace(string(j)))
