3f7ee140e5733ec94fdf02eb6da45670544f467c
James Lucktaylor <james.lucktaylor@cloudreach.com>
2018-02-22 18:39:07 +1100

    Some notes on data structure design

--- 03/01/day3_part1.go
+++ 03/01/day3_part1.go
@@ -1,3 +1,14 @@
 package main
 
 func main() {}
+
+/*
+Distance to travel when adding nodes sequentially: 1, 1, 2, 2, 3, 3, 4... etc. Double check!
+Add, turn, add, turn, ad infinitum.
+
+Coordinates struct: signed integers; negative to left and/or down from origin.
+
+Check bottom right corner with alternating squares: 1, 9, 25, 49, et al
+
+Manhattan distance will be the sum of the two absolute coordinates.
+*/
