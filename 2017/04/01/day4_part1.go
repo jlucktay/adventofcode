4f4c9b825b8287dd9ee39e1a5e322d316761e7de
James Lucktaylor <jlucktay@gmail.com>
2018-02-24 06:38:34 +1100

    Finished part 1 of day 4

--- 2017/04/01/day4_part1.go
+++ 2017/04/01/day4_part1.go
@@ -4,8 +4,45 @@ http://adventofcode.com/2017/day/4
 
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("../input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var tally uint
+
+	for _, p := range strings.Split(string(input[:]), "\n") {
+		if validatePassphrase(p) {
+			tally++
+		}
+	}
+
+	fmt.Printf("Result for day 4, part 1: %d\n", tally)
+}
 
 func validatePassphrase(passphrase string) bool {
-	return false
+	if len(passphrase) == 0 {
+		return false
+	}
+
+	split := strings.Split(passphrase, " ")
+
+	for a, b := range split {
+		for _, d := range split[a+1:] {
+			if b == d {
+				return false
+			}
+		}
+	}
+
+	return true
 }
