2841b806c8a2e7c92dd00eb5805864f2af4d8b9d
James Lucktaylor <jlucktay@gmail.com>
2018-02-24 08:25:17 +1100

    Added some boilerplate input reading to template

--- template/dayX_partX.go
+++ template/dayX_partX.go
@@ -4,7 +4,21 @@ http://adventofcode.com/.../day/...
 
 package main
 
-func main() {}
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("../input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = strings.Split(strings.TrimSpace(string(input[:])), "\n")
+}
 
 func f(b bool) bool {
 	return b
