b197182471490b0b2e268101f8e904d71920922c
James Lucktaylor <jlucktay@gmail.com>
2018-02-23 12:35:50 +1100

    Stubbed out part 1 of day 4

---
+++ 2017/04/01/day4_part1.go
@@ -0,0 +1,11 @@
+/*
+http://adventofcode.com/2017/day/4
+*/
+
+package main
+
+func main() {}
+
+func validatePassphrase(passphrase string) bool {
+	return false
+}
---
+++ 2017/04/01/day4_part1_test.go
@@ -0,0 +1,30 @@
+/*
+For example:
+
+aa bb cc dd ee is valid.
+aa bb cc dd aa is not valid - the word aa appears more than once.
+aa bb cc dd aaa is valid - aa and aaa count as different words.
+*/
+
+package main
+
+import "testing"
+
+func TestValidatePassphrase(t *testing.T) {
+	cases := []struct {
+		in   string
+		want bool
+	}{
+		{"aa bb cc dd ee", true},
+		{"aa bb cc dd aa", false},
+		{"aa bb cc dd aaa", true},
+	}
+
+	for _, c := range cases {
+		got := validatePassphrase(c.in)
+
+		if got != c.want {
+			t.Errorf("validatePassphrase(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
