b595d105bbe102ddeec9d185d3aa2d29436c384d
James Lucktaylor <jlucktay@gmail.com>
2018-02-24 07:12:47 +1100

    Finished day 4, part 2

---
+++ 2017/04/02/day4_part2_test.go
@@ -0,0 +1,34 @@
+/*
+For example:
+
+abcde fghij is a valid passphrase.
+abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+iiii oiii ooii oooi oooo is valid.
+oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+*/
+
+package main
+
+import "testing"
+
+func TestValidatePassphrase(t *testing.T) {
+	cases := []struct {
+		in   string
+		want bool
+	}{
+		{"abcde fghij", true},
+		{"abcde xyz ecdab", false},
+		{"a ab abc abd abf abj", true},
+		{"iiii oiii ooii oooi oooo", true},
+		{"oiii ioii iioi iiio", false},
+	}
+
+	for _, c := range cases {
+		got := validatePassphrase(c.in)
+
+		if got != c.want {
+			t.Errorf("validatePassphrase(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
