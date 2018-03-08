27efbad1b16cd282e5a1f67dd8891144b765837e
James Lucktaylor <jlucktay@gmail.com>
2018-03-08 15:29:18 +0000

    Moved main() functions out to their own .go files

--- template/dayX_partX.go
+++ template/dayX_partX.go
@@ -4,22 +4,6 @@ http://adventofcode.com/.../day/...
 
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"strings"
-)
-
-func main() {
-	input, err := ioutil.ReadFile("../input.txt")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_ = strings.Split(strings.TrimSpace(string(input[:])), "\n")
-}
-
 func f(b bool) bool {
 	return b
 }
