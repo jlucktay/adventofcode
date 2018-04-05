b6347062cd95c0f81ade280b61477885c689f0c3
James Lucktaylor <jlucktay@gmail.com>
2018-04-05 11:41:18 +0100

    Initialise all of the fields in the structs

--- aocautoself/pkg/string.go
+++ aocautoself/pkg/string.go
@@ -7,7 +7,15 @@ import "fmt"
 
 func (d Day) String() string {
 	s := fmt.Sprintf("Advent of Code %d, day %d.\n", d.Year, d.Date)
-	s += d.Description
+	s += fmt.Sprintf("Raw description: \"%s\"\n", d.Description)
+
+	s += fmt.Sprintf("Part 1, fluff: \"%s\"\n", d.Part1.Fluff)
+	s += fmt.Sprintf("Part 1, test: \"%s\"\n", d.Part1.Test)
+	s += fmt.Sprintf("Part 1, stinger: \"%s\"\n", d.Part1.Stinger)
+
+	s += fmt.Sprintf("Part 2, fluff: \"%s\"\n", d.Part2.Fluff)
+	s += fmt.Sprintf("Part 2, test: \"%s\"\n", d.Part2.Test)
+	s += fmt.Sprintf("Part 2, stinger: \"%s\"\n", d.Part2.Stinger)
 
 	return s
 }
