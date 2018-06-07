15785402f0f62ae4dd3699a7ed89d63c77f5b132
James Lucktaylor <jlucktay+github@gmail.com>
2018-06-07 12:47:12 +0100

    Added test based on sample grid given in part 2

--- 2017/03/02/day3_part2_test.go
+++ 2017/03/02/day3_part2_test.go
@@ -12,6 +12,52 @@ package main
 
 import "testing"
 
+func TestLastValue(t *testing.T) {
+	cases := []struct {
+		nodesToAdd uint
+		lastValue  uint
+	}{
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 4},
+		{5, 5},
+		{6, 10},
+		{7, 11},
+		{8, 23},
+		{9, 25},
+		{10, 26},
+		{11, 54},
+		{12, 57},
+		{13, 59},
+		{14, 122},
+		{15, 133},
+		{16, 142},
+		{17, 147},
+		{18, 304},
+		{19, 330},
+		{20, 351},
+		{21, 362},
+		{22, 747},
+		{23, 806},
+	}
+
+	for _, c := range cases {
+		var s spiral
+		s.Init()
+
+		for i := uint(0); i < c.nodesToAdd; i++ {
+			s.Add()
+		}
+
+		got := s.last.value
+
+		if got != c.lastValue {
+			t.Errorf("Value of last node after adding %v nodes == %v, wanted %v", c.nodesToAdd, got, c.lastValue)
+		}
+	}
+}
+
 func TestManhattan(t *testing.T) {
 	cases := []struct {
 		in   int
