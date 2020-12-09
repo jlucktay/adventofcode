bf3b687cd4e0bfe6b9caefac6429e3b6e8711d2b
James Lucktaylor <jlucktay+github@gmail.com>
2020-12-09 17:51:52 +0000

    feat: complete 2020 day 2 part 1

---
+++ 2020/02/01/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, errRead := ioutil.ReadFile("../input.txt")
+	if errRead != nil {
+		fmt.Fprintf(os.Stderr, "could not read input: %v", errRead)
+	}
+
+	validCount, totalCount := 0, 0
+
+	xInput := strings.Split(string(input), "\n")
+	for i := range xInput {
+		xLine := strings.Split(xInput[i], " ")
+
+		xRange := strings.Split(xLine[0], "-")
+
+		if len(xRange) < 2 {
+			continue
+		}
+
+		needle := strings.Split(xLine[1], ":")[0]
+		occurrences := strings.Count(xLine[2], needle)
+
+		totalCount++
+
+		lowerBound, _ := strconv.Atoi(xRange[0])
+		upperBound, _ := strconv.Atoi(xRange[1])
+
+		if occurrences >= lowerBound && occurrences <= upperBound {
+			validCount++
+		}
+	}
+
+	fmt.Printf("Valid/total count: %d/%d\n", validCount, totalCount)
+}
