f7e2fb89d497b3b0a25273894f36300a0dce3c55
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-01 17:58:37 +0000

    refactor(2022): apply fruits of diffuse thinking to day 1

--- 2022/day01/cmd/main.go
+++ 2022/day01/cmd/main.go
@@ -7,16 +7,22 @@ import (
 	"go.jlucktay.dev/adventofcode/2022/day01"
 )
 
+const (
+	EXIT_SUCCESS = iota
+	EXIT_NO_INPUT_FILE_ARG
+	EXIT_CAN_NOT_READ_FILE
+)
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "wrong number of arguments - need an input file")
-		os.Exit(1)
+		os.Exit(EXIT_NO_INPUT_FILE_ARG)
 	}
 
 	input, err := os.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not read file '%s': %v", os.Args[1], err)
-		os.Exit(2)
+		fmt.Fprintf(os.Stderr, "could not read file '%s': %v\n", os.Args[1], err)
+		os.Exit(EXIT_CAN_NOT_READ_FILE)
 	}
 
 	fmt.Println(day01.MostCalories(string(input)))
