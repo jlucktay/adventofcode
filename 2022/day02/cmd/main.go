97387f4661a071e9e5e8e6b0dfc4e7fd57dee033
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-02 07:02:29 +0000

    feat(2022): complete day 2, part 1

---
+++ 2022/day02/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"go.jlucktay.dev/adventofcode/2022/day02"
+)
+
+const (
+	EXIT_SUCCESS = iota
+	EXIT_NO_INPUT_FILE_ARG
+	EXIT_CAN_NOT_READ_FILE
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "wrong number of arguments - need an input file")
+		os.Exit(EXIT_NO_INPUT_FILE_ARG)
+	}
+
+	input, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read file '%s': %v\n", os.Args[1], err)
+		os.Exit(EXIT_CAN_NOT_READ_FILE)
+	}
+
+	fmt.Println(day02.TotalScore(string(input)))
+}
