3043b366afbf3607b8757660f16674db75345231
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-03 06:53:02 +0000

    refactor(2022): dedupe root command code

--- 2022/2022.go
+++ 2022/2022.go
@@ -1,1 +1,27 @@
 package aoc2022
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	EXIT_SUCCESS = iota
+	EXIT_NO_INPUT_FILE_ARG
+	EXIT_CAN_NOT_READ_FILE
+)
+
+func RootCmd() string {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "wrong number of arguments - need an input file")
+		os.Exit(EXIT_NO_INPUT_FILE_ARG)
+	}
+
+	input, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read file '%s': %v\n", os.Args[1], err)
+		os.Exit(EXIT_CAN_NOT_READ_FILE)
+	}
+
+	return string(input)
+}
--- 2022/day02/cmd/main.go
+++ 2022/day02/cmd/main.go
@@ -2,29 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
 
+	aoc2022 "go.jlucktay.dev/adventofcode/2022"
 	"go.jlucktay.dev/adventofcode/2022/day02"
 )
 
-const (
-	EXIT_SUCCESS = iota
-	EXIT_NO_INPUT_FILE_ARG
-	EXIT_CAN_NOT_READ_FILE
-)
-
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "wrong number of arguments - need an input file")
-		os.Exit(EXIT_NO_INPUT_FILE_ARG)
-	}
-
-	input, err := os.ReadFile(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not read file '%s': %v\n", os.Args[1], err)
-		os.Exit(EXIT_CAN_NOT_READ_FILE)
-	}
-
-	fmt.Println(day02.TotalScore(string(input)))
-	fmt.Println(day02.StrategisedScore(string(input)))
+	input := aoc2022.RootCmd()
+	fmt.Println(day02.TotalScore(input))
+	fmt.Println(day02.StrategisedScore(input))
 }
