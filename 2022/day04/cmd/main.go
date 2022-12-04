ae096285ffc3dcbc6745d3216dab9d5fb29b7ded
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-04 08:17:22 +0000

    feat(2022): complete day 4, part 2

--- 2022/day04/cmd/main.go
+++ 2022/day04/cmd/main.go
@@ -10,4 +10,5 @@ import (
 func main() {
 	input := aoc2022.RootCmd()
 	fmt.Println(day04.SectionIDFullyContain(input))
+	fmt.Println(day04.SectionIDOverlap(input))
 }
--- 2022/day04/day04.go
+++ 2022/day04/day04.go
@@ -1,1 +1,57 @@
 package day04
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func sectionIDAnalysis(analyser func(left []int, right []int) bool, input string) (int, error) {
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		xLine := strings.Split(line, ",")
+		if len(xLine) != 2 {
+			return 0, fmt.Errorf("line '%s' is not split by one comma only", line)
+		}
+
+		leftAssignment, err := parseAssignment(xLine[0])
+		if err != nil {
+			return 0, err
+		}
+
+		rightAssignment, err := parseAssignment(xLine[1])
+		if err != nil {
+			return 0, err
+		}
+
+		if analyser(leftAssignment, rightAssignment) {
+			result++
+		}
+	}
+
+	return result, nil
+}
+
+func parseAssignment(input string) ([]int, error) {
+	xInput := strings.Split(input, "-")
+	if len(xInput) != 2 {
+		return nil, fmt.Errorf("string '%s' is not split by one hyphen only", input)
+	}
+
+	left, err := strconv.ParseInt(xInput[0], 10, 32)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := strconv.ParseInt(xInput[1], 10, 32)
+	if err != nil {
+		return nil, err
+	}
+
+	return []int{int(left), int(right)}, nil
+}
