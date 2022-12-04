1d86e3a972fda7573aedb20d412d8c36f2d49226
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-04 07:47:37 +0000

    feat(2022): complete day 4, part 1

---
+++ 2022/day04/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	aoc2022 "go.jlucktay.dev/adventofcode/2022"
+	"go.jlucktay.dev/adventofcode/2022/day04"
+)
+
+func main() {
+	input := aoc2022.RootCmd()
+	fmt.Println(day04.SectionIDFullyContain(input))
+}
