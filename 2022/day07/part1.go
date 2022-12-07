dececccf67e4d5d437268974684537888620d481
James Lucktaylor <jlucktay+github@gmail.com>
2022-12-07 09:26:11 +0000

    feat(2022): complete day 7, part 1

--- 2022/day07/part1.go
+++ 2022/day07/part1.go
@@ -1,5 +1,9 @@
 package day07
 
+import (
+	"path"
+)
+
 func FindDirsUpTo100K(input string) (int, error) {
 	fs, err := ParseFileSystem(input)
 	if err != nil {
@@ -8,7 +12,7 @@ func FindDirsUpTo100K(input string) (int, error) {
 
 	dirSizes := make(map[string]int)
 
-	populateDirSizeMap(dirSizes, fs.root)
+	populateDirSizeMap(dirSizes, fs.root, "/")
 
 	result := 0
 
@@ -21,12 +25,12 @@ func FindDirsUpTo100K(input string) (int, error) {
 	return result, nil
 }
 
-func populateDirSizeMap(dsm map[string]int, dir *directory) {
+func populateDirSizeMap(dsm map[string]int, dir *directory, dirName string) {
 	for name, de := range dir.entries {
-		if de.entryType() == det_dir {
-			dsm[name] = de.totalSize()
-
-			populateDirSizeMap(dsm, de.(*directory))
+		if dir, ok := de.(*directory); ok {
+			thisDirPath := path.Join(dirName, name)
+			dsm[thisDirPath] = dir.totalSize()
+			populateDirSizeMap(dsm, dir, thisDirPath)
 		}
 	}
 }
