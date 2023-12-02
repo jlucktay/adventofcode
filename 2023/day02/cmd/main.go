311349983e7ada7dd54e22e06c85f512e735d069
James Lucktaylor <jlucktay+github@gmail.com>
2023-12-02 05:58:25 +0000

    feat(2023/day02): complete part 1

---
+++ 2023/day02/cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/lmittmann/tint"
+
+	"go.jlucktay.dev/adventofcode/2023/day02"
+)
+
+func main() {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	slog.SetDefault(
+		slog.New(
+			tint.NewHandler(
+				os.Stderr,
+				&tint.Options{
+					TimeFormat: time.RFC3339,
+				})))
+
+	part1, err := day02.CubeConundrum(string(input))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+	}
+
+	slog.Info("part 1", slog.Int("result", part1))
+}
---
+++ 2023/day02/day02_part1.go
@@ -0,0 +1,150 @@
+package day02
+
+import (
+	"fmt"
+	"log/slog"
+	"strconv"
+	"strings"
+)
+
+func CubeConundrum(input string) (int, error) {
+	lines := strings.Split(input, "\n")
+	possibleGames := 0
+
+	cubeColourLimitations := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		games := strings.Split(line, ":")
+
+		if len(games) != 2 {
+			slog.Warn("expecting two parts after splitting on colon; continuing",
+				slog.String("line", line),
+				slog.String("games", fmt.Sprintf("%#v", games)))
+
+			continue
+		}
+
+		var gameNumber int
+
+		scanned, err := fmt.Sscanf(games[0], "Game %d", &gameNumber)
+		if err != nil {
+			return 0, err
+		}
+
+		slog.Debug("scanned numbers",
+			slog.Int("count", scanned),
+			slog.Int("game_number", gameNumber))
+
+		if scanned != 1 {
+			return 0, fmt.Errorf("expecting 1 scanned game number, got %d", scanned)
+		}
+
+		slog.Debug("remainder of line",
+			slog.Int("len", len(games)),
+			slog.String("key", games[1]))
+
+		game := GameCubes{}
+
+		cubeSets := strings.Split(games[1], ";")
+
+		for _, cubeSet := range cubeSets {
+			cubeSet = strings.TrimSpace(cubeSet)
+
+			slog.Debug("cube set",
+				slog.String("cube_set", cubeSet))
+
+			gc := NewGameCube()
+
+			game = append(game, gc)
+
+			cubeColours := strings.Split(cubeSet, ",")
+
+			for _, cubeColour := range cubeColours {
+				cubeColour = strings.TrimSpace(cubeColour)
+
+				slog.Debug("cube colour",
+					slog.String("cube_colour", cubeColour))
+
+				colourCount := strings.Split(cubeColour, " ")
+				if len(colourCount) != 2 {
+					slog.Warn("expecting two fields after split of cube colour;continuing",
+						slog.String("cube_colour", cubeColour))
+
+					continue
+				}
+
+				if err := gc.SetColour(colourCount[1], colourCount[0]); err != nil {
+					slog.Error("setting colour",
+						slog.Any("err", err))
+
+					continue
+				}
+			}
+
+			slog.Debug("game cube",
+				slog.String("key", fmt.Sprintf("%+v", gc)))
+		}
+
+		slog.Debug("game",
+			slog.Int("game_number", gameNumber),
+			slog.String("key", fmt.Sprintf("%+v", game)))
+
+		if game.CheckPossible(cubeColourLimitations) {
+			possibleGames += gameNumber
+		}
+	}
+
+	return possibleGames, nil
+}
+
+type GameCube struct {
+	colours map[string]int
+}
+
+func NewGameCube() GameCube {
+	return GameCube{
+		colours: make(map[string]int),
+	}
+}
+
+func (gc GameCube) SetColour(colour, count string) error {
+	parsedCount, err := strconv.Atoi(count)
+	if err != nil {
+		return err
+	}
+
+	gc.colours[colour] = parsedCount
+
+	return nil
+}
+
+type GameCubes []GameCube
+
+func (gc GameCubes) CheckPossible(limits map[string]int) bool {
+	for _, cube := range gc {
+		for colour, count := range cube.colours {
+			if limits[colour] < count {
+				slog.Warn("not possible",
+					slog.String("cube", fmt.Sprintf("%+v", cube)),
+					slog.String("limits", fmt.Sprintf("%+v", limits)),
+				)
+
+				return false
+			}
+		}
+	}
+
+	slog.Info("possible!",
+		slog.String("limits", fmt.Sprintf("%+v", limits)),
+	)
+
+	return true
+}
