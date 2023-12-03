a0bd83deacd704f0d271659807988dee9a5fc667
James Lucktaylor <jlucktay+github@gmail.com>
2023-12-03 18:45:19 +0000

    feat(2023/day03): complete part 2

--- 2023/day03/day03_part2.go
+++ 2023/day03/day03_part2.go
@@ -1,7 +1,207 @@
-// Package main for Advent of Code ..., day ..., part 2
-// https://adventofcode.com/.../day/...
+// Package main for Advent of Code 2023, day 3, part 2
+// https://adventofcode.com/2023/day/3
 package main
 
+import (
+	"log/slog"
+	"strconv"
+	"unicode"
+	"unicode/utf8"
+)
+
 func Part2(inputLines []string) (int, error) {
-	return 0, nil
+	schematic := [][]rune{}
+	schematicRow := 0
+
+	for _, inputLine := range inputLines {
+		schematic = append(schematic, make([]rune, 0))
+
+		lineIndex := 0
+		inputLineBytes := []byte(inputLine)
+
+		for lineIndex < len(inputLineBytes) {
+			r, size := utf8.DecodeRune(inputLineBytes[lineIndex:])
+
+			schematic[schematicRow] = append(schematic[schematicRow], r)
+
+			lineIndex += size
+		}
+
+		schematicRow++
+	}
+
+	schematicRow = 0
+	result := 0
+
+	for schematicRow < len(schematic) {
+		row := schematic[schematicRow]
+		rowIndex := 0
+
+		for rowIndex < len(row) {
+			switch row[rowIndex] {
+			case '*':
+				slog.Debug("found a gear",
+					slog.Int("row", schematicRow),
+					slog.Int("column", rowIndex))
+
+				gearComponents, err := lookAroundGear(schematic, schematicRow, rowIndex)
+				if err != nil {
+					return 0, err
+				}
+
+				result += gearComponents[0] * gearComponents[1]
+
+			default:
+			}
+
+			rowIndex++
+		}
+
+		schematicRow++
+	}
+
+	return result, nil
+}
+
+func lookAroundGear(schematic [][]rune, row, column int) ([2]int, error) {
+	// Make sure we have at least one row before proceeding.
+	if row < 0 || row >= len(schematic) {
+		return [2]int{}, nil
+	}
+
+	result := [2]int{}
+
+	// Check the row above, if it exists.
+	if row-1 >= 0 {
+		for i := column - 1; i <= column+1; i++ {
+			if i >= 0 && i < len(schematic[row-1]) {
+				if unicode.IsDigit(schematic[row-1][i]) {
+					slog.Debug("number nearby",
+						slog.Int("row", row-1),
+						slog.Int("column", i),
+						slog.String("digit", string(schematic[row-1][i])))
+
+					entireNumber, err := discoverEntireNumber(schematic, row-1, i)
+					if err != nil {
+						return [2]int{}, err
+					}
+
+					if result[0] == 0 {
+						result[0] = entireNumber
+					} else if result[1] == 0 && result[0] != entireNumber {
+						result[1] = entireNumber
+						return result, nil
+					}
+				}
+			}
+		}
+	}
+
+	// Check the same row, before and after, if we're not at the start/end of the row.
+	if column-1 >= 0 {
+		if unicode.IsDigit(schematic[row][column-1]) {
+			slog.Debug("number nearby",
+				slog.Int("row", row),
+				slog.Int("column", column-1),
+				slog.String("digit", string(schematic[row][column-1])))
+
+			entireNumber, err := discoverEntireNumber(schematic, row, column-1)
+			if err != nil {
+				return [2]int{}, err
+			}
+
+			if result[0] == 0 {
+				result[0] = entireNumber
+			} else if result[1] == 0 && result[0] != entireNumber {
+				result[1] = entireNumber
+				return result, nil
+			}
+		}
+	}
+
+	if column+1 < len(schematic[row]) {
+		if unicode.IsDigit(schematic[row][column+1]) {
+			slog.Debug("number nearby",
+				slog.Int("row", row),
+				slog.Int("column", column+1),
+				slog.String("digit", string(schematic[row][column+1])))
+
+			entireNumber, err := discoverEntireNumber(schematic, row, column+1)
+			if err != nil {
+				return [2]int{}, err
+			}
+
+			if result[0] == 0 {
+				result[0] = entireNumber
+			} else if result[1] == 0 && result[0] != entireNumber {
+				result[1] = entireNumber
+				return result, nil
+			}
+		}
+	}
+
+	// Check the row below, if it exists.
+	if row+1 < len(schematic) {
+		for i := column - 1; i <= column+1; i++ {
+			if i >= 0 && i < len(schematic[row+1]) {
+				if unicode.IsDigit(schematic[row+1][i]) {
+					slog.Debug("number nearby",
+						slog.Int("row", row+1),
+						slog.Int("column", i),
+						slog.String("digit", string(schematic[row+1][i])))
+
+					entireNumber, err := discoverEntireNumber(schematic, row+1, i)
+					if err != nil {
+						return [2]int{}, err
+					}
+
+					if result[0] == 0 {
+						result[0] = entireNumber
+					} else if result[1] == 0 && result[0] != entireNumber {
+						result[1] = entireNumber
+						return result, nil
+					}
+				}
+			}
+		}
+	}
+
+	return [2]int{}, nil
+}
+
+func discoverEntireNumber(schematic [][]rune, row, column int) (int, error) {
+	emptyString := ""
+
+	if len(schematic) <= row {
+		return 0, nil
+	}
+
+	// walk to the left
+	if column >= 0 && column < len(schematic[row]) {
+		for i := column; i >= 0; i-- {
+			if unicode.IsDigit(schematic[row][i]) {
+				emptyString = string(schematic[row][i]) + emptyString
+			} else {
+				break
+			}
+		}
+	}
+
+	// walk to the right
+	if column+1 < len(schematic[row]) {
+		for i := column + 1; i < len(schematic[row]); i++ {
+			if unicode.IsDigit(schematic[row][i]) {
+				emptyString = emptyString + string(schematic[row][i])
+			} else {
+				break
+			}
+		}
+	}
+
+	number, err := strconv.Atoi(emptyString)
+	if err != nil {
+		return 0, err
+	}
+
+	return number, nil
 }
--- 2023/day03/day03_part2_test.go
+++ 2023/day03/day03_part2_test.go
@@ -1,5 +1,22 @@
 /*
-For example...
+Consider the same engine schematic again:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+In this schematic, there are two gears.
+The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345.
+The second gear is in the lower right; its gear ratio is 451490.
+(The * adjacent to 617 is not a gear because it is only adjacent to one part number.)
+Adding up all of the gear ratios produces 467835.
 */
 
 package main
@@ -18,6 +35,21 @@ func TestPart2(t *testing.T) {
 		want int
 	}{
 		"empty": {[]string{}, 0},
+		"Consider the same engine schematic again": {
+			[]string{
+				"467..114..",
+				"...*......",
+				"..35..633.",
+				"......#...",
+				"617*......",
+				".....+.58.",
+				"..592.....",
+				"......755.",
+				"...$.*....",
+				".664.598..",
+			},
+			467835,
+		},
 	}
 
 	for desc, testCase := range testCases {
@@ -27,6 +59,130 @@ func TestPart2(t *testing.T) {
 			is := is.New(t)
 
 			got, err := Part2(testCase.in)
+			is.NoErr(err)
+			is.Equal(got, testCase.want)
+		})
+	}
+}
+
+func TestLookAroundGear(t *testing.T) {
+	is := is.New(t)
+
+	schematicRunes := [][]rune{
+		{'4', '6', '7', '.', '.', '1', '1', '4', '.', '.'},
+		{'.', '.', '.', '*', '.', '.', '.', '.', '.', '.'},
+		{'.', '.', '3', '5', '.', '.', '6', '3', '3', '.'},
+		{'.', '.', '.', '.', '.', '.', '#', '.', '.', '.'},
+		{'6', '1', '7', '*', '.', '.', '.', '.', '.', '.'},
+		{'.', '.', '.', '.', '.', '+', '.', '5', '8', '.'},
+		{'.', '.', '5', '9', '2', '.', '.', '.', '.', '.'},
+		{'.', '.', '.', '.', '.', '.', '7', '5', '5', '.'},
+		{'.', '.', '.', '$', '.', '*', '.', '.', '.', '.'},
+		{'.', '6', '6', '4', '.', '5', '9', '8', '.', '.'},
+	}
+
+	testCases := map[string]struct {
+		in          [][]rune
+		row, column int
+		want        [2]int
+	}{
+		"empty": {[][]rune{}, 0, 0, [2]int{0, 0}},
+		"first - second line": {
+			schematicRunes,
+			1, 3,
+			[2]int{467, 35},
+		},
+		"second - fifth line - not actually a gear": {
+			schematicRunes,
+			4, 3,
+			[2]int{0, 0},
+		},
+		"third - ninth line": {
+			schematicRunes,
+			8, 5,
+			[2]int{755, 598},
+		},
+	}
+
+	for desc, testCase := range testCases {
+		desc, testCase := desc, testCase
+
+		t.Run(desc, func(t *testing.T) {
+			is := is.New(t)
+
+			got, err := lookAroundGear(testCase.in, testCase.row, testCase.column)
+			is.NoErr(err)
+			is.Equal(got, testCase.want)
+		})
+	}
+}
+
+func TestDiscoverEntireNumber(t *testing.T) {
+	is := is.New(t)
+
+	schematicRunes := [][]rune{
+		{'4', '6', '7', '.', '.', '1', '1', '4', '.', '.'},
+		{'.', '.', '.', '*', '.', '.', '.', '.', '.', '.'},
+		{'.', '.', '3', '5', '.', '.', '6', '3', '3', '.'},
+		{'.', '.', '.', '.', '.', '.', '#', '.', '.', '.'},
+		{'6', '1', '7', '*', '.', '.', '.', '.', '.', '.'},
+		{'.', '.', '.', '.', '.', '+', '.', '5', '8', '.'},
+		{'.', '.', '5', '9', '2', '.', '.', '.', '.', '.'},
+		{'.', '.', '.', '.', '.', '.', '7', '5', '5', '.'},
+		{'.', '.', '.', '$', '.', '*', '.', '.', '.', '.'},
+		{'.', '6', '6', '4', '.', '5', '9', '8', '.', '.'},
+	}
+
+	testCases := map[string]struct {
+		in          [][]rune
+		row, column int
+		want        int
+	}{
+		"empty": {[][]rune{}, 0, 0, 0},
+		"first - 467": {
+			schematicRunes,
+			0, 0,
+			467,
+		},
+		"second - 114": {
+			schematicRunes,
+			0, 5,
+			114,
+		},
+		"third - 35": {
+			schematicRunes,
+			2, 2,
+			35,
+		},
+		"third - 35 from the 5": {
+			schematicRunes,
+			2, 3,
+			35,
+		},
+		"fourth - 633 0": {
+			schematicRunes,
+			2, 6,
+			633,
+		},
+		"fourth - 633 1": {
+			schematicRunes,
+			2, 7,
+			633,
+		},
+		"fourth - 633 2": {
+			schematicRunes,
+			2, 8,
+			633,
+		},
+	}
+
+	for desc, testCase := range testCases {
+		desc, testCase := desc, testCase
+
+		t.Run(desc, func(t *testing.T) {
+			is := is.New(t)
+
+			got, err := discoverEntireNumber(testCase.in, testCase.row, testCase.column)
 			is.NoErr(err)
 			is.Equal(got, testCase.want)
 		})
