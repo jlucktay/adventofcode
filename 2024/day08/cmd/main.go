646cb39d2421aac0ac121cab0a7de45c696f3162
James Lucktaylor <jlucktay+github@gmail.com>
2024-12-08 16:59:39 +0000

    feat: add time elapsed to today's Advent and the template

--- 2024/day08/cmd/main.go
+++ 2024/day08/cmd/main.go
@@ -24,23 +24,27 @@ func main() {
 		os.Exit(1)
 	}
 
+	startPart1 := time.Now()
 	part1, err := day08.Part1(string(input))
+	finishPart1 := time.Since(startPart1)
 	if err != nil {
 		slog.Error("part 1", slog.Any("err", err))
 		os.Exit(1)
 	}
 
-	slog.Info("part 1", slog.Int("result", part1))
+	slog.Info("part 1", slog.Int("result", part1), slog.Duration("elapsed", finishPart1))
 
 	if part1 == 0 {
 		return
 	}
 
+	startPart2 := time.Now()
 	part2, err := day08.Part2(string(input))
+	finishPart2 := time.Since(startPart2)
 	if err != nil {
 		slog.Error("part 2", slog.Any("err", err))
-		os.Exit(1)
+		os.Exit(2)
 	}
 
-	slog.Info("part 2", slog.Int("result", part2))
+	slog.Info("part 2", slog.Int("result", part2), slog.Duration("elapsed", finishPart2))
 }
