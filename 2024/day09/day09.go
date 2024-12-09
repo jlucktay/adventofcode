0a48f9e0aa4d37fc0d15d28fa5cb5172c35fa90e
James Lucktaylor <jlucktay+github@gmail.com>
2024-12-09 09:11:00 +0000

    feat(2024/day09): complete part 1

---
+++ 2024/day09/day09.go
@@ -0,0 +1,40 @@
+// Package day09 for Advent of Code 2024, day 9.
+// https://adventofcode.com/2024/day/9
+package day09
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strconv"
+)
+
+func parseInput(input string) (Disk, error) {
+	buffer := bytes.NewBufferString(input)
+	scanner := bufio.NewScanner(buffer)
+
+	result := make(Disk, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for index, size := range line {
+			parsedSize, err := strconv.ParseInt(string(size), 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("parsing '%s': %w", string(size), err)
+			}
+
+			for i := 0; i < int(parsedSize); i++ {
+				switch index%2 == 0 {
+				case true:
+					result = append(result, int64(index/2))
+
+				case false:
+					result = append(result, -1)
+				}
+			}
+		}
+	}
+
+	return result, nil
+}
---
+++ 2024/day09/day09_part1.go
@@ -0,0 +1,93 @@
+// Package day09 for Advent of Code 2024, day 9, part 1.
+// https://adventofcode.com/2024/day/9
+package day09
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+type Disk []int64
+
+func (d Disk) String() string {
+	sb := strings.Builder{}
+
+	for _, block := range d {
+		var printLetter rune
+
+		if block == -1 {
+			printLetter = '.'
+		} else if block > 10 {
+			printLetter = 'x'
+		} else {
+			pl, _ := utf8.DecodeRuneInString(fmt.Sprintf("%d", block))
+			printLetter = pl
+		}
+
+		sb.WriteRune(printLetter)
+	}
+
+	return sb.String()
+}
+
+func (d Disk) firstFree() int {
+	for index, block := range d {
+		if block == -1 {
+			return index
+		}
+	}
+
+	return -1
+}
+
+func (d Disk) lastFile() int {
+	for i := len(d) - 1; i >= 0; i-- {
+		if d[i] != -1 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (d Disk) moveSingleBlock(from, to int) {
+	fileID := d[from]
+	d[from] = -1
+	d[to] = fileID
+}
+
+func (d Disk) compact() {
+	if len(d) == 0 {
+		return
+	}
+
+	for d.firstFree() != d.lastFile()+1 {
+		d.moveSingleBlock(d.lastFile(), d.firstFree())
+	}
+}
+
+func (d Disk) checksum() int64 {
+	result := int64(0)
+
+	for i := int64(0); i < int64(len(d))-1; i++ {
+		if d[i] == -1 {
+			break
+		}
+
+		result += i * d[i]
+	}
+
+	return result
+}
+
+func Part1(input string) (int64, error) {
+	d, err := parseInput(input)
+	if err != nil {
+		return 0, err
+	}
+
+	d.compact()
+
+	return d.checksum(), nil
+}
