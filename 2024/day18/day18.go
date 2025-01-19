99ecc16c8a5e10dc505e5d67d8032a5b5ca128fe
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-19 16:02:14 +0000

    feat(2024/day18): complete part 1

--- 2024/day18/day18.go
+++ 2024/day18/day18.go
@@ -3,32 +3,118 @@
 package day18
 
 import (
-	"bufio"
-	"bytes"
-	"slices"
+	"fmt"
+	"image"
 	"strings"
+
+	"github.com/RyanCarrier/dijkstra/v2"
+	"github.com/orsinium-labs/enum"
 )
 
-func parseInput(input string) (struct{}, error) {
-	buffer := bytes.NewBufferString(input)
-	scanner := bufio.NewScanner(buffer)
+type Tile enum.Member[rune]
 
-	result := struct{}{}
+var (
+	Empty = Tile{'.'}
+	Byte  = Tile{'#'}
 
-	for scanner.Scan() {
-		xLine := strings.Split(scanner.Text(), " ")
-		xLine = slices.DeleteFunc(xLine, func(s string) bool { return s == "" })
+	Tiles = enum.New(Empty, Byte)
+)
 
-		// ...
-		// go through input line by line and roll up into result
-		// ...
+type RAMRun struct {
+	grid          map[image.Point]Tile
+	bounds        image.Rectangle
+	byteDropLimit int
+	graph         dijkstra.MappedGraph[image.Point]
+}
 
-    _ = xLine
+func (rr RAMRun) String() string {
+	result := ""
+
+	for y := range rr.bounds.Max.Y {
+		for x := range rr.bounds.Max.X {
+			result += string(rr.grid[image.Pt(x, y)].Value)
+		}
+
+		result += "\n"
 	}
 
-	// ...
-	// validate parsed result
-	// ...
+	return result
+}
+
+func parseInput(input string) (RAMRun, error) {
+	if len(input) == 0 {
+		return RAMRun{byteDropLimit: -1}, nil
+	}
+
+	result := RAMRun{
+		grid:  make(map[image.Point]Tile),
+		graph: dijkstra.NewMappedGraph[image.Point](),
+	}
+
+	xLines := strings.Split(strings.TrimSpace(input), "\n")
+
+	// Slightly different parameters in test and live modes.
+	if len(xLines) <= 25 {
+		result.bounds = image.Rect(0, 0, 7, 7)
+		result.byteDropLimit = 12
+	} else {
+		result.bounds = image.Rect(0, 0, 71, 71)
+		result.byteDropLimit = 1024
+	}
+
+	// Populate with empties to get started.
+	for x := range result.bounds.Max.X {
+		for y := range result.bounds.Max.Y {
+			result.grid[image.Pt(x, y)] = Empty
+
+			if err := result.graph.AddEmptyVertex(image.Pt(x, y)); err != nil {
+				return RAMRun{}, fmt.Errorf("adding empty vertex at %d,%d to graph: %w", x, y, err)
+			}
+		}
+	}
+
+	// Start parsing coordinates of bytes.
+	for i, line := range xLines {
+		var x, y int
+
+		n, err := fmt.Sscanf(line, "%d,%d", &x, &y)
+		if err != nil || n != 2 {
+			return RAMRun{}, fmt.Errorf("scanning line '%s' parsed %d items: %w", line, n, err)
+		}
+
+		result.grid[image.Pt(x, y)] = Byte
+
+		if i+1 >= result.byteDropLimit {
+			break
+		}
+	}
+
+	// Look through the parsed grid for valid neighbours to feed into the graph.
+	for x := range result.bounds.Max.X {
+		for y := range result.bounds.Max.Y {
+			position := image.Pt(x, y)
+
+			for _, neighbour := range parseGetValidNeighbours(result.grid, position) {
+				if err := result.graph.AddArc(position, neighbour, 1); err != nil {
+					return RAMRun{}, fmt.Errorf("adding arc between %s and %s: %w", position, neighbour, err)
+				}
+			}
+		}
+	}
 
 	return result, nil
+}
+
+func parseGetValidNeighbours(grid map[image.Point]Tile, position image.Point) []image.Point {
+	result := make([]image.Point, 0)
+
+	for _, direction := range []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+		candidate := position.Add(direction)
+
+		if tileType, ok := grid[candidate]; ok && tileType == Empty {
+			result = append(result, candidate)
+		}
+	}
+
+	return result
 }
--- 2024/day18/day18_part1.go
+++ 2024/day18/day18_part1.go
@@ -2,13 +2,44 @@
 // https://adventofcode.com/2024/day/18
 package day18
 
+import (
+	"fmt"
+	"image"
+	"strconv"
+)
+
 func Part1(input string) (int, error) {
-	_, err := parseInput(input)
+	rr, err := parseInput(input)
 	if err != nil {
 		return 0, err
 	}
 
-	result := 0
+	return rr.MinimumStepsToExit()
+}
 
-	return result, nil
+func (rr RAMRun) MinimumStepsToExit() (int, error) {
+	start := image.Pt(0, 0)
+
+	var finish image.Point
+
+	switch rr.byteDropLimit {
+	case -1:
+		return 0, nil
+
+	case 12:
+		finish = image.Pt(6, 6)
+
+	case 1024:
+		finish = image.Pt(70, 70)
+
+	default:
+		panic("unknown byte drop limit: " + strconv.Itoa(rr.byteDropLimit))
+	}
+
+	path, err := rr.graph.Shortest(start, finish)
+	if err != nil {
+		return 0, fmt.Errorf("finding shortest path: %w", err)
+	}
+
+	return int(path.Distance), nil
 }
