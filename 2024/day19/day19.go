e528f16c9b01e372cdcaf111b96fa0357eb11761
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-22 16:40:52 +0000

    chore(2024/day19): templates

---
+++ 2024/day19/day19.go
@@ -0,0 +1,34 @@
+// Package day19 for Advent of Code 2024, day 19.
+// https://adventofcode.com/2024/day/19
+package day19
+
+import (
+	"bufio"
+	"bytes"
+	"slices"
+	"strings"
+)
+
+func parseInput(input string) (struct{}, error) {
+	buffer := bytes.NewBufferString(input)
+	scanner := bufio.NewScanner(buffer)
+
+	result := struct{}{}
+
+	for scanner.Scan() {
+		xLine := strings.Split(scanner.Text(), " ")
+		xLine = slices.DeleteFunc(xLine, func(s string) bool { return s == "" })
+
+		// ...
+		// go through input line by line and roll up into result
+		// ...
+
+    _ = xLine
+	}
+
+	// ...
+	// validate parsed result
+	// ...
+
+	return result, nil
+}
---
+++ 2024/day19/day19_part2.go
@@ -0,0 +1,14 @@
+// Package day19 for Advent of Code 2024, day 19, part 2.
+// https://adventofcode.com/2024/day/19
+package day19
+
+func Part2(input string) (int, error) {
+	_, err := parseInput(input)
+	if err != nil {
+		return 0, err
+	}
+
+	result := 0
+
+	return result, nil
+}
